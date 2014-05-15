f7fc0403bf8f2bccb52952a01429968962bfdda6
Greg Mercer <gmercer@stanford.edu>
2014-05-15 11:00:33 -0700

    Update loadpoll.js

--- routes/loadpoll.js
+++ routes/loadpoll.js
@@ -31,6 +31,87 @@ exports.loadpoll = function(req, res) {
 
 };
 
+function loadPoll_saveAnswer(testNumber, answer) {
+
+  // poll answers are for example s1r1 - s1r11
+  //
+  // "pollAnswers" : [ 
+  //  { "pollId" : "survey1", "answer" : "s1r1" }, 
+  //  { "pollId" : "survey2", "answer" : "s2r1" }, 
+  //  { "pollId" : "survey3", "answer" : "s3r5" }, 
+  //  { "pollId" : "survey4", "answer" : "s4r6", 
+  //      "explainAnswer" : "Some of...", 
+  //      "explainAnswer2" : "I focused..." 
+  //  } 
+  // ]
+
+  var pollAnswer = answer.answer;
+  var pos = pollAnswer.indexOf('r');
+  console.log("pos = " + pos);
+  pollAnswer = pollAnswer.substr(pos+1);
+  console.log("pollAnswer = " + pollAnswer);      
+  pollAnswer = parseInt(pollAnswer, 10);
+  pollAnswer = pollAnswer - 1;
+
+  var insert = "INSERT INTO " + result_table + 
+    " (TestNumber, PollQuestion, PollAnswer) VALUES(" + 
+    "'" + testNumber + "', " + 
+    "'" + answer.pollId + "', " +        
+    "'" + pollAnswer + "')";
+    
+  client.query(insert,
+    function selectCb(err, results, fields) {
+      if (err) { throw err; }
+      console.log('insert complete');
+    }
+  );
+    
+  console.log("insert = " + insert);  
+
+  if (answer.explainAnswer) {
+
+    console.log('in ')
+
+    pollAnswer = answer.explainAnswer.replace(/'/g, " ");
+    console.log('explainAnswer = '+pollAnswer);
+
+    var insert = "INSERT INTO " + result_table + 
+      " (TestNumber, PollQuestion, PollAnswer) VALUES(" + 
+      "'" + testNumber + "', " + 
+      "'" + 'explainAnswer' + "', " +        
+      "'" + pollAnswer + "')";
+      
+    client.query(insert,
+      function selectCb(err, results, fields) {
+        if (err) { throw err; }
+        console.log('insert complete');
+      }
+    );
+
+  }
+
+  if (answer.explainAnswer2) {
+
+    pollAnswer = answer.explainAnswer2.replace(/'/g, " ");
+    console.log('explainAnswer2 = '+pollAnswer);
+
+    var insert = "INSERT INTO " + result_table + 
+      " (TestNumber, PollQuestion, PollAnswer) VALUES(" + 
+      "'" + testNumber + "', " + 
+      "'" + 'explainAnswer2' + "', " +        
+      "'" + pollAnswer + "')";
+      
+    client.query(insert,
+      function selectCb(err, results, fields) {
+        if (err) { throw err; }
+        console.log('insert complete');
+      }
+    );
+
+  }
+
+}
+
 function loadPollResults() {
 
   resultsloop: for (index in results.all) {
@@ -41,30 +122,7 @@ function loadPollResults() {
     
       var answer = result.pollAnswers[answer_index];
 
-      // poll answers are for example s1r1 - s1r11
-
-      var pollAnswer = answer.answer;
-      var pos = pollAnswer.indexOf('r');
-      console.log("pos = " + pos);
-      pollAnswer = pollAnswer.substr(pos+1);
-      console.log("pollAnswer = " + pollAnswer);      
-      pollAnswer = parseInt(pollAnswer, 10);
-      pollAnswer = pollAnswer - 1;
-
-      var insert = "INSERT INTO " + result_table + 
-        " (TestNumber, PollQuestion, PollAnswer) VALUES(" + 
-        "'" + result.testNumber + "', " + 
-        "'" + answer.pollId + "', " +        
-        "'" + pollAnswer + "')";
-        
-        client.query(insert,
-          function selectCb(err, results, fields) {
-            if (err) { throw err; }
-            console.log('insert complete');
-          }
-        );
-            
-        console.log("insert = " + insert);  
+      loadPoll_saveAnswer(result.testNumber, answer);
 
     } // end answersloop
     
