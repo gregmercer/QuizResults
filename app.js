71d254754552d0c74c734d522bc9e1b5e419e4c3
Greg Mercer <gmercer@stanford.edu>
2014-05-16 15:57:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ var express = require('express')
 var index = require("./routes/index");
 var loadquestion = require("./routes/loadquestion");
 var loadword = require("./routes/loadword");
+var loadwordtime = require("./routes/loadwordtime");
 var loadtime = require("./routes/loadtime");
 var loadpoll = require("./routes/loadpoll");
+var loadphase = require("./routes/loadphase");
 
 var app = express();
 
@@ -39,9 +41,13 @@ app.get('/loadQuestionResults', loadquestion.loadquestion);
 
 app.get('/loadWordResults', loadword.loadword);
 
+app.get('/loadWordTimeResults', loadwordtime.loadwordtime);
+
 app.get('/loadTimeResults', loadtime.loadtime);
 
 app.get('/loadPollResults', loadpoll.loadpoll);
+
+app.get('/loadPhaseResults', loadphase.loadphase);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
