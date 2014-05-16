35148506e689705066aeeced00f13209d4cfcf97
Greg Mercer <gmercer@stanford.edu>
2014-05-16 16:01:28 -0700

    Create QuizPhaseResults.sql

---
+++ QuizPhaseResults.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `quiz_story_phase_results` (
+  `TestNumber` varchar(20) DEFAULT NULL,
+  `StoryType` varchar(20) DEFAULT NULL,
+  `Completed_In_Phase_One` varchar(255) DEFAULT NULL,
+  `Completed_In_Phase_Two` varchar(255) DEFAULT NULL, 
+  `Time_In_Phase_Three` varchar(255) DEFAULT NULL 
+);
