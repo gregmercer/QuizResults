356edf43627596f2b3fecf795e25c6eca5072cc8
Greg Mercer <gmercer@stanford.edu>
2014-05-16 16:09:40 -0700

    Update QuizWordResults.sql

--- QuizWordResults.sql
+++ QuizWordResults.sql
@@ -1,17 +1,19 @@
 TestNumber,SelectedWords,CorrectWords,NumCorrect,NonItems,NumNonItems,IncorrectlyMarked,NumIncorrectlyMarked
-TestNumber, NumberDistractorWords, NumberCorrectWords, NumberIncorrectWords, SelectedWords, DistractorWords, CorrectWords, IncorrectWords
+TestNumber, NumberDistractorWords, NumberConfusionWords, NumberCorrectWords, NumberIncorrectWords, SelectedWords, DistractorWords, ConfusionWords, CorrectWords, IncorrectWords
 
-TestNumber, NumberDistractorWords, NumberCorrectlySelected, NumberIncorrectlySelected, NumberCorrectlyIndentified, 
-SelectedWords, DistractorWords, CorrectlySelectedWords, IncorrectlySelectedWords, CorrectlyIndentifiedWords
+TestNumber, NumberDistractorWords, NumberConfusionWords, NumberCorrectlySelected, NumberIncorrectlySelected, NumberCorrectlyIndentified, 
+SelectedWords, DistractorWords, ConfusionWords, CorrectlySelectedWords, IncorrectlySelectedWords, CorrectlyIndentifiedWords
 
 CREATE TABLE `quiz_word_results` (
   `TestNumber` varchar(4096) DEFAULT NULL,
   `NumberDistractorWords` text DEFAULT NULL,  
+  `NumberConfusionWords` text DEFAULT NULL,  
   `NumberCorrectlySelected` text DEFAULT NULL,
   `NumberIncorrectlySelected` text DEFAULT NULL,
   `NumberCorrectlyIndentified` text DEFAULT NULL,
   `SelectedWords` text DEFAULT NULL,
   `DistractorWords` text DEFAULT NULL,  
+  `ConfusionWords` text DEFAULT NULL,  
   `CorrectlySelectedWords` text DEFAULT NULL,
   `IncorrectlySelectedWords` text DEFAULT NULL,  
   `CorrectlyIndentifiedWords` text DEFAULT NULL    
